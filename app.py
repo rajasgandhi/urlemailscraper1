957394b8d733f693b2e748bc9fc2b6a0d4742ffd
RajasG <rajasmgandhi@gmail.com>
2020-06-25 13:53:06 -0500

    update

--- app.py
+++ app.py
@@ -84,5 +84,5 @@ async def fetch(url, session):
     return re.findall("([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)", r.html.html)
 
 if __name__ == "__main__":
-    #app.run(host='0.0.0.0', port=int(os.environ.get('PORT',5000)))
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT',5000)))
+    #app.run(debug=True)
